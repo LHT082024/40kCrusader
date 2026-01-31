a0696dc9749e0d0025b182b3f68e10fb56a56672
LHT082024 <LHT082024@gmail.com>
2026-01-31 22:08:16 +0100

    made two placeholder variables for the armies and a string that prints when we run the program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿using System; 
+﻿using System;
 
 class Program
 {
-    static void Main ()
+    static void Main()
     {
-        
+        string armyMain = "lol";
+        string army1 = "lol2";
+       
+        Console.WriteLine($"Welcome to the crusader tracker, which army are we looking at today ?\n{armyMain}\n{army1}");
     }
 }
