2534a7b1cd108e6cddc78e33d8a295d42d7e6e80
LHT082024 <LHT082024@gmail.com>
2026-01-31 23:24:58 +0100

    updated starter text

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ class Program
         string armyMain = "lol";
         string army1 = "lol2";
 
-        Console.WriteLine($"Welcome to the crusader tracker :)\n are we \nAdding squad type\nEditing an army?\ntype: Y\\N for yes or no");
+        Console.WriteLine($"Welcome to the crusader tracker :)\n are we \nAdding squad type\nEditing an army?\nType: 1 for adding Unit\nType 2 for editing an army");
         UnitAdding.GiveUnitName();
-        
+
     }
 }
